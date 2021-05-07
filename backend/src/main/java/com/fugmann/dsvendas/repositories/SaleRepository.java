697f9120b7a32386f5d960000542d9a5932b2219
Stefan Fugmann <stefan.fugmann@outlook.com>
2021-05-07 01:21:30 -0300

    Pagination

---
+++ backend/src/main/java/com/fugmann/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.fugmann.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fugmann.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
