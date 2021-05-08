a5e13232ac9c11e55384fa3535c88ff0bba67af3
Stefan Fugmann <stefan.fugmann@outlook.com>
2021-05-08 01:27:08 -0300

    Group by search

--- backend/src/main/java/com/fugmann/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/fugmann/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.fugmann.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fugmann.dsvendas.dto.SaleDTO;
+import com.fugmann.dsvendas.dto.SaleSuccessDTO;
+import com.fugmann.dsvendas.dto.SaleSumDTO;
 import com.fugmann.dsvendas.sevice.SaleService;
 
 @RestController
@@ -24,4 +28,15 @@ public class SaleController {
 		return ResponseEntity.ok(list);
 	}
 	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
 }
--- backend/src/main/java/com/fugmann/dsvendas/sevice/SaleService.java
+++ backend/src/main/java/com/fugmann/dsvendas/sevice/SaleService.java
@@ -1,5 +1,7 @@
 package com.fugmann.dsvendas.sevice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fugmann.dsvendas.dto.SaleDTO;
+import com.fugmann.dsvendas.dto.SaleSuccessDTO;
+import com.fugmann.dsvendas.dto.SaleSumDTO;
 import com.fugmann.dsvendas.entities.Sale;
 import com.fugmann.dsvendas.repositories.SaleRepository;
 import com.fugmann.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
